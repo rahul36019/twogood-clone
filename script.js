ad3d47ccc860456e8718ac89908219ffa07a3662
Rahul kumar <rahulkumarmr360@gmail.com>
2025-07-24 23:48:57 +0530

    add page1 heading animation on initial load

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 console.log("clone of a twogood website");
- // here we are making bubble play button effect
-let videoContainer = document.getElementsByClassName("video_container")[0];
-let playBtn = document.getElementsByClassName("play")[0];
 
-videoContainer.addEventListener("mouseenter", function () {
-    gsap.to(playBtn, {
-        scale: 1,
-        opacity: 1,
+function videoAnimation() {
+    // here we are making bubble play button effect
+    let videoContainer = document.getElementsByClassName("video_container")[0];
+    let playBtn = document.getElementsByClassName("play")[0];
+
+    videoContainer.addEventListener("mouseenter", function () {
+        gsap.to(playBtn, {
+            scale: 1,
+            opacity: 1,
+        });
     });
-});
 
 
-videoContainer.addEventListener("mouseleave", function () {
-    gsap.to(playBtn, {
-        scale: 0,
-        opacity: 0,
+    videoContainer.addEventListener("mouseleave", function () {
+        gsap.to(playBtn, {
+            scale: 0,
+            opacity: 0,
+        });
     });
-});
 
-videoContainer.addEventListener("mousemove", function (dets) {
-    gsap.to(playBtn, {
-        left: dets.x-50,
-        top : dets.y-50, 
+    videoContainer.addEventListener("mousemove", function (dets) {
+        gsap.to(playBtn, {
+            left: dets.x - 50,
+            top: dets.y - 50,
+        });
     });
-});
+}
+
+function loadingAnimation(){
+    gsap.from(".page1 h1",{
+        y:100,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.3
+    })
+}
+
+
+loadingAnimation();
+videoAnimation();
